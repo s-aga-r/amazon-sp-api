7ee223e4956d34352f6fcd28afb7981dadb50a47
Sagar Sharma <sagarsharma.s312@gmail.com>
2021-12-24 23:52:03 +0530

    feat: config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# https://sellercentral.amazon.in/marketplacedeveloper/applications
+APP_ID = "amzn1.sp.solution.61c1fd0c-927f-4e1e-87b1-************"
+IAM_ARN = "arn:aws:iam::187442139888:**************"
+CLIENT_ID = "amzn1.application-oa2-client.********************************"
+CLIENT_SECRET = "10c8c4cfbd5eb5247b08465a2eef6657911*****************************"
+REFRESH_TOKEN = "Atzr|IwEBILv1IcazwpgeMB6eJ7MgjDP2kfSFXeDcRP2ytXSYnzN4K5Rb8xN39DnSOyFiqi-hA1VuXOwhYmEltcpsPjAL37cxzUAJrshRTVRHkXMXDIE3MjdMArXcXAlLgSB-Pv1FIW3-2Q-MsAEwyWhil50terjLeUptLnNvYhBS0isSMftwAfyM2oMKaQfmi2xahA2YwOa6QbUukdGCdIOHQwkLqq_UilXAOqxym8uKFGUUkyse_p_ZP-*********************************************************************************"
+
+# https://console.aws.amazon.com/iamv2/home#/users
+AWS_ACCESS_KEY = "AKIASXJDMC**********"
+AWS_SECRET_KEY = "qdQKp1BDlxKcFIbRLsHpU0J*****************"
+
+# https://github.com/amzn/selling-partner-api-docs/blob/main/guides/en-US/developer-guide/SellingPartnerApiDeveloperGuide.md#selling-partner-api-endpoints
+SELLING_REGION = "Europe"
+COUNTRY_CODE = "IN"
+
+MAX_RETRY_LIMIT = 10
+
